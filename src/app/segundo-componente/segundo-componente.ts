5ccb0b866f6233ff62b1be7a8f91a980dec5553e
Kesia Pimenta <kesiapimentaferreira525@gmail.com>
2026-04-12 10:33:51 -0300

    arquivos iniciais do projeto

---
+++ src/app/segundo-componente/segundo-componente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  imports: [],
+  templateUrl: './segundo-componente.html',
+  styleUrl: './segundo-componente.css',
+})
+export class SegundoComponente {}
